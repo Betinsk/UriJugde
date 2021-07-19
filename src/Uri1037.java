1c2d776caa4227378289ae1d4a93ca3809fc43b5
Betinsk <gmdust@hotmail.com>
2021-07-19 20:42:36 -0300

    Uri 1067

--- src/Uri1037.java
+++ src/Uri1037.java
@@ -11,18 +11,18 @@ public class Uri1037 {
 		double x = sc.nextDouble();
 		
 		
-		if (x >= 0 && x < 25) {
+		if (x >= 0 && x <= 25) {
 			System.out.println("Intervalo [0,25]");
 		}
 		
-		else if (x >= 25 && x < 50) {
+		else if (x > 25 && x <= 50) {
 			System.out.println("Intervalo (25,50]");
 		}
-		else if (x >= 50 && x < 75) {
+		else if (x > 50 && x <=75) {
 			System.out.println("Intervalo (50,75]");
 		}
 		
-		else if (x >= 75 && x <= 100) {
+		else if (x >75 && x <= 100) {
 			System.out.println("Intervalo (75,100]");
 		}
 		else {
---
+++ src/Uri1067.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Uri1067 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		
+		int z = 1;
+		while(z <=x) {
+			System.out.println(z);
+			z+=2;
+		}
+		
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
