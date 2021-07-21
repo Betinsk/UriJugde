ab20c3284a9eb3eab8478942be8ee41f647cad0c
Betinsk <gmdust@hotmail.com>
2021-07-21 20:10:22 -0300

    Nova versao

--- src/Uri1070.java
+++ src/Uri1070.java
@@ -6,9 +6,22 @@ public class Uri1070 {
 		Scanner sc = new Scanner(System.in);
 
 		int x = sc.nextInt();
+			
+		int cont = 0;
+		
+		while(cont < 6) {
+			if(x %2 ==1) {
+				System.out.println(x);
+				cont ++;
+			}
+			x++;
+		}
 		
 		
-		for(int i =0; i<6; i++) {
+		
+		
+		
+	/*	for(int i =0; i<6; i++) {
 			
 			if(x %2==0) {
 				x += 2;
@@ -22,7 +35,7 @@ public class Uri1070 {
 			}
 
 		sc.close();
-
+*/
 	}
 
 }
